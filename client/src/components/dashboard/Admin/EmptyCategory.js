ccc73afda66b61b7a4ebeea9f1763910483c855c
richa <devteamri@gmail.com>
2023-03-09 10:31:22 +0530

    Richa EmptyCat display

--- client/src/components/dashboard/Admin/EmptyCategory.js
+++ client/src/components/dashboard/Admin/EmptyCategory.js
@@ -1,11 +1,94 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 
-export default function EmptyCategory() {
+import { getAllCategory } from "../../../actions/dag";
+import Addcategory from "./AddCategory";
+import EditCategory from "./EditCategory";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+//import { loadUser } from "../../../actions/auth";
+
+const EmptyCategory = ({
+  auth: { user },
+  dag: { allcat },
+
+  getAllCategory,
+}) => {
+  useEffect(() => {
+    if (user) {
+      getAllCategory(user.orgId);
+    }
+  }, []);
+  // console.log("user data on cat",user)
+  const onClickReset = () => {
+    if (user) {
+      getAllCategory(user.orgId);
+    }
+  };
+
+  //deactivate
+  const [formData, setFormData] = useState({
+    category_DE_Reason: "",
+    isSubmitted: false,
+  });
+  const { category_DE_Reason } = formData;
+
+  //deactivate modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [catId, setCatId] = useState("");
+
+  const onDelete = (id) => {
+    setCatId(id);
+    handleShow();
+  };
+
+  let history = useHistory();
+  const onJoinCat = (data) => {
+    history.push("/joinleavecat", data);
+  };
+
+  const onInputchange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  //deactivate
+  const onAdd = (e) => {
+    e.preventDefault();
+    const reason = {
+      catid: catId,
+      catdeletereason: category_DE_Reason,
+      orgId: user.orgId,
+      DeactiveById: user._id,
+      DeactiveByName: user.fullName,
+      DeactiveByDateTime: new Date().toLocaleString("en-GB"),
+    };
+
+    getAllCategory(user.orgId);
+    handleClose();
+  };
+
+  //edit modal
+  const [showEditModal, setShowEditModal] = useState(false);
+  const handleEditModalClose = () => setShowEditModal(false);
+  const handleOpen = () => setShowEditModal(true);
+
+  const [catdata, setcatdata] = useState(null);
+  const onedit = (user2) => {
+    setShowUpdateModal(true);
+    setcatdata(user2);
+    handleOpen();
+  };
+
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const handleUpdateModalClose = () => {
+    setShowUpdateModal(false);
+    getAllCategory(user.orgId);
+  };
+
   return (
     <div>
       <div className="row">
@@ -24,7 +107,7 @@ export default function EmptyCategory() {
               <img
                 className="img_icon_size log text-right"
                 src={require("../../../static/images/refresh-icon.png")}
-                //onClick={() => onClickReset()}
+                onClick={() => onClickReset()}
                 alt="refresh"
                 title="Refresh"
               />{" "}
@@ -37,18 +120,250 @@ export default function EmptyCategory() {
                 id="datatable2"
                 className="table table-bordered table-hover table-striped"
               >
-                <tr className="headingsizes">
-                  <th style={{ width: "50%" }}>Category Name</th>
-                  <th style={{ width: "40%" }}>Description</th>
+                <thead>
+                  <tr className="headingsizes">
+                    <th style={{ width: "50%" }}>Category Name</th>
+                    <th style={{ width: "40%" }}>Description</th>
 
-                  <th style={{ width: "10%" }}>Operation</th>
-                </tr>
-                <tr></tr>
+                    <th style={{ width: "10%" }}>Operation</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allcat &&
+                    allcat.map((catVal, idx) => {
+                      if (
+                        catVal.categoryEntity &&
+                        catVal.categoryEntity.length == 0 &&
+                        catVal.categoryStatus == "Active"
+                      ) {
+                        return (
+                          <tr key={idx}>
+                            <td>{catVal.categoryName}</td>
+                            <td>{catVal.categoryDesp}</td>
+
+                            <td>
+                              {catVal.categoryStatus == "Active" ? (
+                                <>
+                                  <img
+                                    className="img_icon_size log"
+                                    onClick={() => onJoinCat(catVal)}
+                                    src={require("../../../static/images/account1.png")}
+                                    alt="Join Leave"
+                                    title="Join Leave Page"
+                                  />
+                                  {/* </Link> */}
+                                </>
+                              ) : (
+                                <>Deactivated</>
+                              )}
+                            </td>
+                          </tr>
+                        );
+                      }
+                    })}
+                </tbody>
               </table>
             </div>
           </section>
         </div>
       </div>
     </div>
+    // <div>
+    //   <div className="row">
+    //     <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-left">
+    //       <br />
+    //       <br />
+    //       <section>
+    //         <h1
+    //           style={{ fontFamily: "Serif", color: "#877bae" }}
+    //           className="font-weight-bold "
+    //         >
+    //           Category Lists
+    //         </h1>
+
+    //         <div className="col-lg-12 col-md-12 col-sm-12 col-12  text-right mb-2">
+    //           <img
+    //             className="img_icon_size log text-right"
+    //             src={require("../../../static/images/refresh-icon.png")}
+    //             onClick={() => onClickReset()}
+    //             alt="refresh"
+    //             title="Refresh"
+    //           />{" "}
+    //           &nbsp;
+    //           <Addcategory />
+    //         </div>
+
+    //         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center body-inner no-padding table-responsive fixTableHead">
+    //           <table
+    //             border="1"
+    //             id="datatable2"
+    //             className="table-striped table table-bordered table-hover"
+    //           >
+    //             <thead>
+    //               <tr className="headingsizes">
+    //                 <th style={{ width: "40%" }}>Category Name</th>
+    //                 <th style={{ width: "40%" }}>Description</th>
+
+    //                 <th style={{ width: "20%" }}>Operation</th>
+    //               </tr>
+    //             </thead>
+    //             <tbody>
+    //               {allcat &&
+    //                 allcat.map((catVal, idx) => {
+    //                   return (
+    //                     <tr key={idx}>
+    //                       <td>{catVal.categoryName}</td>
+    //                       <td>{catVal.categoryDesp}</td>
+
+    //                       <td>
+    //                         {catVal.categoryStatus == "Active" ? (
+    //                           <>
+    //                             <img
+    //                               className="img_icon_size log"
+    //                               onClick={() => onedit(catVal)}
+    //                               src={require("../../../static/images/edit_icon.png")}
+    //                               alt="Edit"
+    //                               title="Edit Category"
+    //                             />
+    //                             &nbsp;
+    //                             <img
+    //                               className="img_icon_size log"
+    //                               onClick={() => onDelete(catVal._id)}
+    //                               src={require("../../../static/images/delete.png")}
+    //                               alt="Delete User"
+    //                               title="Delete Category"
+    //                             />
+    //                             &nbsp;
+    //                             {/* <Link to="#"> */}{" "}
+    //                             <img
+    //                               className="img_icon_size log"
+    //                               onClick={() => onJoinCat(catVal)}
+    //                               src={require("../../../static/images/account1.png")}
+    //                               alt="Join Leave"
+    //                               title="Join Leave Page"
+    //                             />
+    //                             {/* </Link> */}
+    //                           </>
+    //                         ) : (
+    //                           <>Deactivated</>
+    //                         )}
+    //                       </td>
+    //                     </tr>
+    //                   );
+    //                 })}
+    //             </tbody>
+    //           </table>
+    //           <div className="text-right ">
+    //             No. of Categories:{allcat && allcat.length}
+    //           </div>
+    //         </div>
+    //       </section>
+    //     </div>
+    //   </div>
+
+    //   {/* edit modal */}
+    //   <Modal
+    //     show={showUpdateModal}
+    //     backdrop="static"
+    //     keyboard={false}
+    //     size="lg"
+    //     aria-labelledby="contained-modal-title-vcenter"
+    //     centered
+    //   >
+    //     <Modal.Header>
+    //       <div className="col-lg-10">
+    //         <h3 className="modal-title text-center">Edit Category</h3>
+    //       </div>
+    //       {/* <Modal.Title className="container">
+    //         <h1 className="font-weight-bold ">Edit Category</h1>
+    //       </Modal.Title> */}
+    //       <div className="col-lg-2">
+    //         <button onClick={handleUpdateModalClose} className="close">
+    //           <img
+    //             src={require("../../../static/images/close.png")}
+    //             alt="X"
+    //             style={{ height: "20px", width: "20px" }}
+    //           />
+    //         </button>
+    //       </div>
+    //     </Modal.Header>
+    //     <Modal.Body>
+    //       {/* <EditUser userdata={orgdata} closeedit={handleUpdateModalClose} /> */}
+    //       <EditCategory
+    //         categorydata={catdata}
+    //         closeedit={handleUpdateModalClose}
+    //       />
+    //     </Modal.Body>
+    //   </Modal>
+
+    //   {/*DEACTIVATE MODAL */}
+
+    //   <Modal
+    //     show={show}
+    //     // onHide={handleClose}
+    //     centered
+    //     size="lg"
+    //     aria-labelledby="contained-modal-title-vcenter"
+    //   >
+    //     <Modal.Header>
+    //       {/* <Modal.Title className="container">
+    //         <h1 className="font-weight-bold ">DEACTIVATE </h1>
+    //       </Modal.Title> */}
+    //       <div className="col-lg-11 ">
+    //         <h3 className="modal-title text-center">DEACTIVATE</h3>
+    //       </div>
+    //       <div className="col-lg-1">
+    //         <button onClick={handleClose} className="close">
+    //           <img
+    //             src={require("../../../static/images/close.png")}
+    //             alt="X"
+    //             style={{ height: "20px", width: "20px" }}
+    //           />
+    //         </button>
+    //       </div>
+    //     </Modal.Header>
+    //     <Modal.Body>
+    //       <form onSubmit={(e) => onAdd(e)}>
+    //         <div className="row col-lg-12 col-md-9 col-sm-9 col-12 ">
+    //           <div className="col-lg-12 col-md-4 col-sm-4 col-12">
+    //             <label>Reason for deactivation</label>
+    //           </div>
+    //           <div className="col-lg-12 col-md-4 col-sm-4 col-12 text-center">
+    //             <textarea
+    //               rows="3"
+    //               name="category_DE_Reason"
+    //               onChange={(e) => onInputchange(e)}
+    //               id="org_reason"
+    //               className="textarea form-control"
+    //               required
+    //             ></textarea>
+    //           </div>
+    //           <div className="col-lg-12 col-md-4 col-sm-4 col-12 py-2">
+    //             <label>Are you sure you want to deactive ?</label>
+    //           </div>
+    //           <div className=" col-lg-12 col-md-9 col-sm-9 col-12 text-right">
+    //             <button
+    //               className="btn btn-outline-secondary btnall"
+    //               type="submit"
+    //             >
+    //               {" "}
+    //               DEACTIVATE
+    //             </button>
+    //           </div>
+    //         </div>
+    //       </form>
+    //     </Modal.Body>
+    //   </Modal>
+    // </div>
   );
-}
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  dag: state.dag,
+});
+export default connect(mapStateToProps, {
+  //   getallcatGroup,
+  // deleteCategory,
+  // loadUser,
+  getAllCategory,
+})(EmptyCategory);
--- client/src/components/dashboard/Admin/JoinLeaveCat.js
+++ client/src/components/dashboard/Admin/JoinLeaveCat.js
@@ -6,61 +6,71 @@ import Modal from "react-bootstrap/Modal";
 import { useHistory } from "react-router-dom";
 //import { loadUser } from "../../../actions/auth";
 
-const JoinLeaveCat = ({ auth: { user }, dag: { allent }, location,getAllCategory }) => {
-
+const JoinLeaveCat = ({
+  auth: { user },
+  dag: { allent },
+  location,
+  getAllCategory,
+}) => {
   const myuser = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
     if (myuser) {
       getAllCategory(myuser.orgId);
-     
     }
   }, []);
   const history = useHistory();
   const [mydata, setmydata] = useState(location.state);
-  const[showjoin,setShoejoin]=useState(false)
+  const [showjoin, setShoejoin] = useState(false);
+  console.log("datafromemtycat", mydata);
 
   const ent = allent;
 
-  const [entCatMembers, setEntCatMembers] = useState(mydata.categoryEntity)
+  const [entCatMembers, setEntCatMembers] = useState(mydata.categoryEntity);
 
-  const [notMember, setNotMember] = useState( ent.filter(ele=>!entCatMembers.some(item=>item["_id"]===ele["_id"]))); 
-  console.log("not a mem",notMember)
+  const [notMember, setNotMember] = useState(
+    ent.filter(
+      (ele) => !entCatMembers.some((item) => item["_id"] === ele["_id"])
+    )
+  );
+  console.log("not a mem", entCatMembers);
 
   const JoinEnt = (ele, index) => {
-    setEntCatMembers([...entCatMembers, ele])
+    setEntCatMembers([...entCatMembers, ele]);
 
     const updated = notMember.filter((elem, id) => {
       return index !== id;
     });
-    setNotMember(updated)
-  }
+    setNotMember(updated);
+  };
 
-  const LeaveEnt=(ele,index)=>{
-
-    setNotMember([...notMember, ele])
+  const LeaveEnt = (ele, index) => {
+    setNotMember([...notMember, ele]);
 
     const updated = entCatMembers.filter((elem, id) => {
       return index !== id;
     });
 
-    setEntCatMembers(updated)
-  }
+    setEntCatMembers(updated);
+  };
 
-const savent=()=>{
-  //console.log("inside save",mydata._id, " user org id",user.orgId);
+  const savent = () => {
+    //console.log("inside save",mydata._id, " user org id",user.orgId);
 
-  var linkPath = "";
+    var linkPath = "";
 
-  axios.post(`${linkPath}/api/category/addCategoryEnt`, {
-     "catid":mydata._id,
-     "orgId":user.orgId,
-     "categoryEntity":entCatMembers,
-    })
-    .then((res)=>{console.log(res)})
-    setShoejoin(false)
+    axios
+      .post(`${linkPath}/api/category/addCategoryEnt`, {
+        catid: mydata._id,
+        orgId: user.orgId,
+        categoryEntity: entCatMembers,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    setShoejoin(false);
     history.push("/category");
-}
+  };
 
   return (
     <div>
@@ -73,30 +83,34 @@ const savent=()=>{
                   <div className="row form-group">
                     <div className="col-md-10 col-lg-10 col-sm-10 col-xs-9 no-pad">
                       <h3>
-                        Join or Leave For {" "+mydata.categoryName+" "} category
+                        Join or Leave For {" " + mydata.categoryName + " "}{" "}
+                        category
                         <span style={{ color: "#ff9500" }}></span>
                       </h3>
                     </div>
 
                     <div className="col-lg-12 col-md-12 col-sm-12 col-12  text-right">
-                    <button  className="btn btn-outline-secondary btn-sm mx-3"
-                    onClick={() =>setShoejoin(true)}
-                    style={{border:"1px solid black"}} >Save</button>
+                      <button
+                        className="btn btn-outline-secondary btn-sm mx-3"
+                        onClick={() => setShoejoin(true)}
+                        style={{ border: "1px solid black" }}
+                      >
+                        Save
+                      </button>
                       <img
                         className="img_icon_size log"
                         src={require("../../../static/images/back.png")}
                         alt="Add User"
                         title="Back"
-                        onClick={() =>{ history.push("/category")}}
+                        onClick={() => {
+                          history.push("/category");
+                        }}
                       />
                     </div>
                     <br />
                   </div>
                   <div className="row">
-                    <div
-                      className="col-md-6 col-lg-6 col-sm-12 col-xs-12 padding_right table-responsive fixTableHeadjoin"
-
-                    >
+                    <div className="col-md-6 col-lg-6 col-sm-12 col-xs-12 padding_right table-responsive fixTableHeadjoin">
                       {/* <div className="body-inner table-responsive padding_col_xs">  */}
                       <table
                         className="table table-bordered table-striped table-hover"
@@ -127,17 +141,25 @@ const savent=()=>{
                         </thead>
                         <tbody>
                           {notMember.map((ele, index) => {
-                            return <tr key={index}>
-                              <td><button onClick={() => JoinEnt(ele, index)}>+</button></td>
-                              <td>{ele.entName}</td>
-                              <td>{ele.entType}</td>
-                              <td> <button>Op</button></td>
-                            </tr>
+                            return (
+                              <tr key={index}>
+                                <td>
+                                  <button onClick={() => JoinEnt(ele, index)}>
+                                    +
+                                  </button>
+                                </td>
+                                <td>{ele.entName}</td>
+                                <td>{ele.entType}</td>
+                                <td>
+                                  {" "}
+                                  <button>Op</button>
+                                </td>
+                              </tr>
+                            );
                           })}
-
                         </tbody>
                       </table>
-                      
+
                       <label
                         style={{
                           fontsize: "18px",
@@ -180,14 +202,22 @@ const savent=()=>{
                         </thead>
                         <tbody>
                           {entCatMembers.map((ele, index) => {
-                            return <tr key={index}>
-                              <td><button onClick={() => LeaveEnt(ele, index)}>-</button></td>
-                              <td>{ele.entName}</td>
-                              <td>{ele.entType}</td>
-                              <td> <button>Op</button></td>
-                            </tr>
+                            return (
+                              <tr key={index}>
+                                <td>
+                                  <button onClick={() => LeaveEnt(ele, index)}>
+                                    -
+                                  </button>
+                                </td>
+                                <td>{ele.entName}</td>
+                                <td>{ele.entType}</td>
+                                <td>
+                                  {" "}
+                                  <button>Op</button>
+                                </td>
+                              </tr>
+                            );
                           })}
-
                         </tbody>
                       </table>
                       {/* <ul className="pagination pagination-sm" style={{margintop: "1px"}}>
@@ -205,7 +235,6 @@ const savent=()=>{
                       </label>
                     </div>
                   </div>
-
                 </section>
               </div>
             </div>
@@ -214,35 +243,31 @@ const savent=()=>{
       </div>
 
       <Modal
-          show={showjoin}
-          backdrop="static"
-          keyboard={false}
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-          className="logout-modal"
-        >
-          <Modal.Header className="confirmbox-heading">
-            <h4 className="mt-0">Confirmation</h4>
-          </Modal.Header>
-          <Modal.Body>
-            <h5>Are you sure you want to join this Entity?</h5>
-          </Modal.Body>
-          <Modal.Footer>
-            <button
-              className="btn contact_reg btn_color"
-              onClick={savent} 
-            >
-              YES
-            </button>
-            <button
-              className="btn contact_reg btn_color"
-              onClick={() =>setShoejoin(false)}
-            >
-              NO
-            </button>
-          </Modal.Footer>
-        </Modal>
-
+        show={showjoin}
+        backdrop="static"
+        keyboard={false}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        className="logout-modal"
+      >
+        <Modal.Header className="confirmbox-heading">
+          <h4 className="mt-0">Confirmation</h4>
+        </Modal.Header>
+        <Modal.Body>
+          <h5>Are you sure you want to join this Entity?</h5>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn contact_reg btn_color" onClick={savent}>
+            YES
+          </button>
+          <button
+            className="btn contact_reg btn_color"
+            onClick={() => setShoejoin(false)}
+          >
+            NO
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
